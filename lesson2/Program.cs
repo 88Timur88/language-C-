aebda35c19c8c19b65372355c8e60302c222bdb6
Fox <gazizov-1988@list.ru>
2022-12-04 19:06:50 +0500

    init work

---
+++ lesson2/Program.cs
@@ -0,0 +1,18 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Напишите программу, которая на вход принимает два числа и проверяет,
+// является ли первое число квадратом второго
+
+int numberA = int.Parse(Console.ReadLine());
+int numberB = int.Parse(Console.ReadLine());
+
+int work = numberB*numberB;
+
+if(work == numberA)
+{
+    Console.WriteLine("Yes");       
+}
+else
+{
+    Console.Write("No");
+}
+
