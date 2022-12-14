8eb47ed1628d5ea60c72d081a1ddf684fb30246d
Fox <gazizov-1988@list.ru>
2022-12-14 23:35:23 +0500

    Added work on Lesson c# 3

---
+++ lesson3.3/Program.cs
@@ -0,0 +1,43 @@
+﻿// SeeНапишите программу, которая принимает на вход
+// координаты двух точек и находит расстояние между ними в 2D пространстве.
+// 
+// A (3,6); B (2,1) -> 5,09 A (7,-5); B (1,-1) -> 7,21
+/*Console.WriteLine("Введите координаты точки А");
+bool isParsedX1 = double.TryParse(Console.ReadLine(), out double x1);
+bool isParsedY1 = double.TryParse(Console.ReadLine(), out double y1);
+
+Console.WriteLine("Введите координаты точки В");
+bool isParsedX2 = double.TryParse(Console.ReadLine(), out double x2);
+bool isParsedY2 = double.TryParse(Console.ReadLine(), out double y2);
+
+if(!isParsedX1 && !isParsedY1 && !isParsedX2 && !isParsedY2)
+{
+    Console.WriteLine("Координаты ввели не корректно");
+}
+
+
+double b = (x2-x1)*(x2-x1);
+double c = (y2-y1)*(y2-y1);
+
+double distance = GetDistanceBetweenPoints(x1, y1, x2, y2);
+Console.WriteLine(distance);
+
+if(b != c && b != 0 && c != 0)
+{
+    int distance = Math.Sqrt(b + c);
+}
+return;*/
+
+int x1 = int.Parse(Console.ReadLine());
+int y1 = int.Parse(Console.ReadLine());
+
+int x2 = int.Parse(Console.ReadLine());
+int y2 = int.Parse(Console.ReadLine());
+
+double result = GetDistanceBetweenPoints(x1, y1, x2, y2);
+Console.WriteLine(result);
+
+double GetDistanceBetweenPoints(int x1, int y1, int x2, int y2)
+{
+    return Math.Round(Math.Sqrt(Math.Pow(x2 - x1, 2)+ Math.Pow(y2 - y1, 2)), 2);
+}
---
+++ lesson3.4/Program.cs
@@ -0,0 +1,44 @@
+﻿/*
+Задача 22: Напишите программу, которая принимает на
+вход число (N) и выдаёт таблицу квадратов чисел от 1 до N.
+int[] array =
+
+5 -> 1, 4, 9, 16, 25. 2 -> 1,4
+*/
+
+int InputInt()
+{
+    Console.WriteLine("ВВедите целое число");
+    bool isNum = int.TryParse(Console.ReadLine(), out int num);
+    if (isNum)
+    {
+        return num;
+    }
+    else
+    {
+        Console.WriteLine("Вы ввели некорректное значение");
+        return -1;
+    }
+}
+int num = InputInt();
+
+int[] GetTableOfSquares(int num)
+{
+    int[] arr = new int[num];
+        for(int i = 0; i < arr.Length; i++)
+        {
+            arr[i] = (int)(Math.Pow(i+1, 2));
+        }
+return arr;
+}
+
+int[] squaresTable = GetTableOfSquares(num);
+
+void PrintArray(int[] arr)
+{
+    for (int i = 0; i < arr.Length; i++)
+    {
+        Console.Write($"{arr[i]} ");
+    }
+}
+PrintArray(squaresTable);
